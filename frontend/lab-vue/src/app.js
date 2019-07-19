0b7c7586f35eea96aef05229564f422e6cdaf0a7
lucas ferraro <lferraro@fiqus.com>
2019-07-19 17:17:24 -0300

    add vuelidate for validations

--- frontend/lab-vue/src/app.js
+++ frontend/lab-vue/src/app.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import app from './app.vue';
 import router from './router'
 
 Vue.config.silent = true; // https://vuejs.org/v2/api/#silent
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
