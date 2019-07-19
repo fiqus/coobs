941dd51b7abb1bab2e93cbfbd984b5b4be86d65b
lucas ferraro <lferraro@fiqus.com>
2019-07-19 17:45:08 -0300

    login screen ready

---
+++ frontend/lab-vue/src/components/input-form.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-group">
+    <label>
+      {{label}}
+    </label>
+    <input class="form-control"
+      :class="{'is-invalid': error}"
+      :name="name"
+      :type="type"
+      v-model="value"
+      @input="onInput"/>
+    <div v-if="error" class="invalid-feedback">
+      {{errorMessage}}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    type: {
+      String,
+      default: "text"
+    },
+    label: {
+      type: String
+    },
+    error: {
+      type: Boolean,
+      default: false
+    },
+    errorMessage: {
+      type: String
+    }
+  },
+  methods: {
+    onInput() {
+      this.$emit('input', this.value);
+    }
+  }
+}
+</script>
+
--- frontend/lab-vue/src/screens/login.vue
+++ frontend/lab-vue/src/screens/login.vue
@@ -3,15 +3,22 @@
     <div class="row">
       <div class="col-sm-4 offset-sm-4">
         <form v-on:submit.prevent="submit" class="needs-validation" novalidate>
-          <div class="form-group">
-            <label>Email: </label>
-            <input type="email" v-model="user.email" class="form-control" />
-            <div class="invalid-feedback"></div>
-          </div>
-          <div class="form-group">
-            <label>Password: </label>
-            <input type="password" v-model="user.password" class="form-control" />
-          </div>
+          <input-form
+            name="email"
+            type="email"
+            v-model="user.email"
+            label="Email: "
+            :error="$v.user.email.$error"
+            error-message="Ingrese un email válido">
+          </input-form>
+          <input-form
+            name="password"
+            type="password"
+            v-model="user.password"
+            label="Password: "
+            :error="$v.user.password.$error"
+            error-message="Ingrese un password válido">
+          </input-form>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
@@ -20,21 +27,42 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
+  import InputForm from '../components/input-form.vue'
+  import { required, email } from 'vuelidate/lib/validators';
+
+  export default {
+    components: {
+      InputForm
+    },
+    data() {
+      return {
+        user: {
+          email: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      submit(aa) {
+        this.$v.$touch();
+        if (!this.$v.$invalid) {
+          // TODO perform login action
+          console.log(`Email: ${this.user.email}`);
+          console.log(`Password: ${this.user.password}`);
+        }
+      }
+    },
+    validations: {
       user: {
-        email: "",
-        pass: ""
+        email: {
+          required,
+          email
+        },
+        password: {
+          required
+        }
       }
     }
-  },
-  methods: {
-    submit(aa) {
-      console.log(`Email: ${this.user.email}`);
-      console.log(`Password: ${this.user.password}`);
-    }
   }
-}
 </script>
 
