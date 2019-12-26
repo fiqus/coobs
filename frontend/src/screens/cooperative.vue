40d4da019c05b1fe0c3a390638b23a8d4bf44be9
lucas ferraro <lferraro@fiqus.com>
2019-12-26 13:23:58 -0300

    #46 use logged in user's cooperative id

--- frontend/src/screens/cooperative.vue
+++ frontend/src/screens/cooperative.vue
@@ -7,7 +7,7 @@
     </div>
     <form v-on:submit.prevent="submit" class="col-lg-6 needs-validation" novalidate>
       <input-form
-        label="Name"
+        :label="$t('name')"
         name="name"
         type="text"
         v-model="cooperative.name"
@@ -15,7 +15,7 @@
       </input-form>
 
       <input-form
-        label="Business name"
+        :label="$t('businessName')"
         name="business name"
         type="text"
         v-model="cooperative.business_name"
@@ -23,15 +23,15 @@
         error-message="Required">
       </input-form>
       <datepicker-form
-        label="Starting date"
+        :label="$t('startingDate')"
         name="starting date"
         format="dd/MM/yyyy"
         v-model="cooperative.starting_date"
         @input="onDateSelected('from', $event)">
       </datepicker-form>
       <div>
-				<button type="button" class="btn btn-secondary" @click.stop="$router.go(-1)"><i class="fa fa-arrow-left"></i> Cancel</button>
-				<button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Save</button>
+				<button type="button" class="btn btn-secondary" @click.stop="$router.go(-1)"><i class="fa fa-arrow-left"></i> {{$t("cancel")}}</button>
+				<button type="submit" class="btn btn-success"><i class="fa fa-save"></i> {{$t("save")}}</button>
 			</div>
     </form>
   </div>
@@ -43,6 +43,7 @@
   import {required} from "vuelidate/lib/validators";
   import {httpGet, httpPut} from "../api-client.js";
   import swal from 'sweetalert';
+  import {getUser} from '../services/user-service';
 
   export default {
     components: {
@@ -50,28 +51,26 @@
       "datepicker-form": DatePickerForm
     },
     created() {
-      // TODO deberíamos usar el cooperative id del usuario logueado
-      // ${this.$route.params.cooperativeId}
-      httpGet(`/cooperatives/1`)
+      httpGet(`/cooperatives/${this.user.cooperative}`)
         .then((response) => {
           this.cooperative = response.data;
         });
     },
     data() {
       return {
+        user: getUser(),
         cooperative: {},
-        title: "Edit cooperative"
+        title: this.$t("editCooperative")
       }
     },
     methods: {
       onDateSelected(dateField, value) {
         this.cooperative[`starting_date_${dateField}`] = new Date(value).toISOString().slice(0,10);
-      },      
+      },
       submit() {
         this.$v.$touch();
         if (!this.$v.$invalid) {
-          // TODO deberíamos usar el cooperative id del usuario logueado
-          httpPut(`/cooperatives/1/`, this.cooperative)
+          httpPut(`/cooperatives/${this.user.cooperative}/`, this.cooperative)
             .then(() => {
               swal("The cooperative has been edited!", {
                 icon: "success",
