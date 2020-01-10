976e7b85588c8e659354dd1f3b09a64aecc4bb01
belcattaneo <belcattaneo@fiqus.coop>
2020-01-10 11:47:08 -0300

    Fixed periods budgets

--- frontend/src/screens/periods/list.vue
+++ frontend/src/screens/periods/list.vue
@@ -42,9 +42,9 @@ export default {
     return {
       headers: [
         {key: "name", value: this.$t("name"), parser: (p) => formatText(p.name)},
-        {key: "dateFrom", value: this.$t("from"), parser: (p) => formatText(p.date_from, 50)},
-        {key: "dateTo", value: this.$t("to"), parser: (p) => formatText(p.date_to, 50)},
-        {key: "actionsBudget", value: this.$t("budget"), parser: (p) => parseBoolean(p.actions_budget)},
+        {key: "dateFrom", value: this.$t("from"), parser: (p) => formatText(p.dateFrom, 50)},
+        {key: "dateTo", value: this.$t("to"), parser: (p) => formatText(p.dateTo, 50)},
+        {key: "actionsBudget", value: this.$t("budget"), parser: (p) => parseBoolean(p.actionsBudget)},
       ],
       periods: []
     };
