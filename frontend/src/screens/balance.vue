8327ff4b1d52489ef98050bc85df695ff163bbff
martin.vallone <martin.vallone@fiqus.com>
2020-01-17 15:30:04 -0300

    [MJV] Generate balance PDF #7

--- frontend/src/screens/balance.vue
+++ frontend/src/screens/balance.vue
@@ -7,25 +7,27 @@
       </h3>
     </div>
     <div v-else>
-      <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h3 class="h5 mb-0 text-gray-800">
-          {{$t("balanceSubtitle", {period: period.name, from: period.dateFrom, to: period.dateTo, budget: Number(period.actionsBudget)})}}
-        </h3>
-        <button type="button" class="btn btn-primary">{{$t("downloadBalance")}} <i class="fas fa-file-download"/></button>
+      <button type="button" class="btn btn-primary float-right my-n2" v-on:click="download">{{$t("downloadBalance")}} <i class="fas fa-file-download"/></button>
+      <div id="nodeToRenderAsPDF">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+          <h3 class="h5 mb-0 text-gray-800">
+            {{$t("balanceSubtitle", {period: period.name, from: period.dateFrom, to: period.dateTo, budget: Number(period.actionsBudget)})}}
+          </h3>
+        </div>
+
+        <balance-by-period-table v-for="(periodSummary, idx) in actionsByPeriod" :key="idx"
+          :period-summary="periodSummary">
+        </balance-by-period-table>
+
+        <table class="table table-hover">
+          <thead>
+            <tr class="row table-info h5">
+              <th class="col-sm-10" scope="colgroup" colspan="4">{{$t("totalInvested")}}</th>
+              <th class="col-sm-2" scope="col">${{totalInvested}}</th>
+            </tr>
+          </thead>
+        </table>
       </div>
-
-      <balance-by-period-table v-for="(periodSummary, idx) in actionsByPeriod" :key="idx"
-        :period-summary="periodSummary">
-      </balance-by-period-table>
-
-      <table class="table table-hover">
-        <thead>
-          <tr class="row table-info h5">
-            <th class="col-sm-10" scope="colgroup" colspan="4">{{$t("totalInvested")}}</th>
-            <th class="col-sm-2" scope="col">${{totalInvested}}</th>
-          </tr>
-        </thead>
-      </table>
     </div>
   </div>
 </template>
@@ -33,11 +35,40 @@
 <script>
   import {httpGet} from '../api-client';
   import BalanceByPeriodTable from '../components/balance-by-period-table.vue';
+  import html2pdf from 'html2pdf.js';
+
+  function print(period){
+    const html = $('#nodeToRenderAsPDF')[0];
+    const opt = {
+      margin:       [5, 5, 5, 5],
+      filename:     `Balance - ${period.name}.pdf`,
+      image:        { type: 'jpg', quality: 0.98 },
+      html2canvas:  { scale: 1 },
+      jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait'},
+      pagebreak:    {mode: ['avoid-all','css']}
+    };
+
+    html2pdf().from(html).set(opt).toPdf().get('pdf').then((pdf) => {
+      var totalPages = pdf.internal.getNumberOfPages();
+
+      for (let i = 1; i <= totalPages; i++) {
+        pdf.setPage(i);
+        pdf.setFontSize(10);
+        pdf.setTextColor(150);
+        pdf.text('Page '+i+' of '+totalPages, pdf.internal.pageSize.width/2-10, pdf.internal.pageSize.height - 5);
+      } 
+    }).save();
+  }
 
   export default {
     components: {
       BalanceByPeriodTable
     },
+    methods:{
+      download() {
+        print(this.period);
+      }
+    },    
     created() {
       return httpGet('/dashboard')
         .then((res) => {
