b99f71bb31959cddff50847c1c5c66229fd63970
martin.vallone <martin.vallone@fiqus.com>
2020-01-27 16:03:43 -0300

    [MJV] Update generate_fake_actions since now actions can be associated with multiple principles #99

--- api/management/commands/generate_fake_actions.py
+++ api/management/commands/generate_fake_actions.py
@@ -5,24 +5,35 @@ from api.models import Cooperative, Action, Principle, Partner
 
 
 class Command(BaseCommand):
-    help = "command used to generate test actions"
+    help = "command used to generate test actions, receives coop_id, quantity, and a boolean value to indicate if you would like to associate multiple principles to each action (the quantity of principles to associate is generated randomly) "
 
     def add_arguments(self, parser):
-        parser.add_argument('coop_id', type=int)
-        parser.add_argument('quantity', type=int)
+        parser.add_argument('coop_id', type=int, help='for which cooperative id you want to add actions?')
+        parser.add_argument('quantity', type=int, help='how many actions do you want to create?')
+        parser.add_argument('associate_multiple_principles', type=int, default=0, help=' indicate 1 if you would like to associate multiple principles to each action (the quantity of principles to associate is generated randomly)')
 
     def handle(self, *args, **options):
         try:
-            principles = list(Principle.objects.all())
-            partners = list(Partner.objects.all())
+            principles = list(Principle.objects.filter(cooperative=options['coop_id']))
+            partners = list(Partner.objects.filter(cooperative=options['coop_id']))
             investments = random.sample(range(1, 1000), options['quantity'])
             coop = Cooperative.objects.get(pk=options['coop_id'])
 
             for n in range(options['quantity']):
                 action = Action(date=datetime.date.today(), name="Action {}".format(n),
                                 description="Action {} description".format(n), invested_money=random.choice(investments),
-                                cooperative=coop, public=True,
-                                principle=random.choice(principles))
+                                cooperative=coop, public=True)
+                action.save()
+                associated_principles = []
+                if options['associate_multiple_principles']:
+                    random.seed()
+                    quantity = random.randint(1, principles.__len__()-1)
+                    for i in range(quantity):
+                        associated_principles.append(random.choice(principles))
+                else:
+                    associated_principles.append(random.choice(principles))
+
+                action.principles.set(associated_principles)
                 action.save()
 
                 if partners:
