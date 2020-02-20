2c7da2fd2784c285bb9fdc3759f93fed359f6e59
belcattaneo <belcattaneo@fiqus.coop>
2020-02-20 15:09:33 -0300

    Quick view for actions

--- frontend/src/screens/actions/list.vue
+++ frontend/src/screens/actions/list.vue
@@ -20,15 +20,37 @@
             </button>
           </div>
           <div class="modal-body">
-            <label>{{$t('name')}}</label>
+            <label class="bold">{{$t('name')}}:</label>
             <span name="name"
-              type="text">{{modalAction.name}}
+              type="text">{{modalAction.actionData.name}}
             </span><br/>
-            <label>{{$t('description')}}</label>
+            <label class="bold">{{$t('description')}}:</label>
             <span name="description"
               type="text">
-              {{modalAction.description}}
-            </span>
+              {{modalAction.actionData.description}}
+            </span><br/>
+            <label class="bold">{{$t('principles')}}:</label><br/>
+            <span class="multiselect__tag" v-for="principle in modalAction.principles" v-bind:key="principle" 
+              name="principles" type="text">
+              {{$t(principle.nameKey)}}
+            </span><br/>
+            <label class="bold">{{$t('partners')}}:</label><br/>
+            <span class="multiselect__tag" v-for="partner in modalAction.actionData.partnersSelected" v-bind:key="partner" 
+              name="partners" type="text">
+              {{partner}}
+            </span><br/>
+            <label class="bold">{{$t('startingDate')}}:</label>
+            <span name="startingDate"
+              type="text">{{modalAction.actionData.date}}
+            </span><br/>
+            <label class="bold">{{$t('investedHours')}}:</label>
+            <span name="investedHours"
+              type="text">{{modalAction.actionData.investedHours}}
+            </span><br/>
+            <label class="bold">{{$t('investedMoney')}}:</label>
+            <span name="investedMoney"
+              type="text">$ {{modalAction.actionData.investedMoney}}
+            </span><br/>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -96,7 +118,7 @@ import {httpGet, httpDelete} from "../../api-client.js";
 import CustomTable from "../../components/custom-table.vue";
 import Loader from "../../components/loader-overlay.vue";
 import ActionQuickView from "../../components/action-quick-view.vue";
-import {formatText, capitalizeFirstChar} from "../../utils";
+import {formatText, capitalizeFirstChar, principlesSelectedParser} from "../../utils";
 import swal from "sweetalert";
 import * as api from "./../../services/api-service";
 import SelectForm from "../../components/select-form.vue";
@@ -206,7 +228,7 @@ export default {
       },
       isLoading: true,
       emptyMsg: this.$t('emptyActionMsg'),
-      modalAction: {}
+      modalAction: {actionData:{}, principles: {}}
     };
   },
   methods: {
@@ -301,8 +323,13 @@ export default {
         api.getPrinciples(),
         api.getAction(action.id)
       ]).then(([principles, actionData]) => {
-        console.log(principles, actionData);
-        this.modalAction = actionData;
+        console.log(JSON.stringify(actionData));
+        let principlesSelected = principlesSelectedParser(actionData.principles, principles)
+        console.log(JSON.stringify(principlesSelected))
+        actionData.partnersSelected = actionData.partnersInvolved.map((partner) => {
+          return `${capitalizeFirstChar(partner.firstName)} ${capitalizeFirstChar(partner.lastName)}`
+        }); 
+        this.modalAction = {actionData: actionData, principles: principlesSelected};
         $('#actionDetailModal').modal()
       });
 
