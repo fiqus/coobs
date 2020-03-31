bcd7f90457ccf6fa83b4d692fbd4a0cc61a32ece
lucas ferraro <lferraro@fiqus.com>
2020-03-31 19:22:42 -0300

    #100 fix landing files for correct webpack building

---
+++ frontend/landing/app.js
@@ -0,0 +1,12 @@
+import './vendor/jquery/jquery.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/jquery-easing/jquery.easing.min.js';
+import './vendor/translate/jquery.tr.js';
+import './vendor/translate/translations.js';
+import './js/jqBootstrapValidation.js';
+import './js/contact_me.js';
+import './js/freelancer.min.js';
+
+import './vendor/fontawesome-free/css/all.min.css';
+import './css/freelancer.min.css';
+import './css/custom.css';
