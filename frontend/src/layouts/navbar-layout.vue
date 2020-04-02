af5631a9798b6b5076d26e0d771b9b1811f65eb8
Diego Calero <dcalero@fiqus.coop>
2020-04-02 20:22:02 -0300

    Updating year to 2020 (ideally it should be automatically parsed with the current year).

--- frontend/src/layouts/navbar-layout.vue
+++ frontend/src/layouts/navbar-layout.vue
@@ -139,7 +139,7 @@
         <footer class="sticky-footer bg-white">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <span>{{$t("createdByFiqus")}} | COOBS 2019</span>
+              <span>{{$t("createdByFiqus")}} | COOBS 2020</span>
             </div>
           </div>
         </footer>
