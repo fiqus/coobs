a38126cde3488991bc28c11261a0c861d965990e
lucas ferraro <lferraro@fiqus.com>
2020-04-24 16:22:36 -0300

    initialize sustainableDevelopmentGoals

--- frontend/src/screens/actions/edit.vue
+++ frontend/src/screens/actions/edit.vue
@@ -210,7 +210,8 @@ export default {
     const isNew = this.$route.params.actionId == "0";
     return {
       action: {
-        principles: "",
+        principles: [],
+        sustainableDevelopmentGoals: [],
         public: true
       },
       principles: [],
