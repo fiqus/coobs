8c20fa79252a415c24c6b1627ce7cf4487c8d528
lucas ferraro <lferraro@fiqus.com>
2020-05-07 15:14:06 -0300

    fix cooperative update

--- frontend/src/screens/cooperative.vue
+++ frontend/src/screens/cooperative.vue
@@ -58,7 +58,7 @@ import DatePickerForm from "../components/datepicker-form.vue";
 import BootstrapToggle from "vue-bootstrap-toggle";
 import swal from "sweetalert";
 import {required} from "vuelidate/lib/validators";
-import {httpGet, httpPut} from "../api-client.js";
+import {httpGet} from "../api-client.js";
 import ErrorForm from "../components/error-form.vue";
 import errorHandlerMixin from "./../mixins/error-handler";
 
@@ -87,40 +87,20 @@ export default {
     onDateSelected(newDate) {
       this.cooperative.startingDate = new Date(newDate).toISOString().slice(0, 10);
     },
-    created() {
-      httpGet(`/cooperatives/${this.cooperativeId}`)
-        .then((response) => {
-          this.cooperative = response.data;
-        });
-    },
-    data() {
-      return {
-        cooperativeId: this.$store.state.user.cooperativeId,
-        cooperative: {},
-        title: "editCooperative"
-      };
-    },
     submit() {
       this.$v.$touch();
       if (!this.$v.$invalid) {
-        httpPut(`/cooperatives/${this.cooperativeId}/`, this.cooperative)
+        this.$store.dispatch("updateCooperative", this.cooperative)
           .then(() => {
             swal(this.$t("editedCoopMsg"), {
               icon: "success",
               buttons: false,
               timer: 2000
             });
-            this.$store.commit("setCooperative",  this.cooperative);
-            this.$router.push({name: "cooperative"});
           })
           .catch((err) => {
             this.handleError(err);
           });
-      }
-    },
-    validations: {
-      cooperative: {
-        businessName: {required}
       }
     }
   },
--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,7 +1,15 @@
+import {httpPut} from "../api-client.js";
+
 export const actions = {
   logout({commit}) {
     commit("setUser", {});
     commit("setCooperative", {});
     commit("setLang", "");
+  },
+  updateCooperative({commit}, cooperative) {
+    httpPut(`/cooperatives/${cooperative.id}/`, cooperative)
+      .then((response) => {
+        commit("setCooperative", response.data);
+      })
   }
 };
