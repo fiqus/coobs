e2e4233748172db0b9b741d2f67d0fa8387d3386
lucas ferraro <lferraro@fiqus.com>
2020-05-22 17:02:15 -0300

    #198 collapse balance panels

--- frontend/src/components/balance-by-period-table.vue
+++ frontend/src/components/balance-by-period-table.vue
@@ -2,18 +2,18 @@
 <div class="container-fluid">
   <table class="table table-hover">
     <thead>
-      <tr class="row thead-light">
+      <tr class="row thead-light cursorPointer" @click="expanded = !expanded">
         <th v-if="groupedBy === 'sdg'" class="col-sm-12" scope="colgroup" colspan="4">{{$t(periodSummary.objectiveNameKey, periodSummary.objectiveNameKey)}}</th>
         <th v-if="groupedBy !== 'sdg'" class="col-sm-12" scope="colgroup" colspan="4">{{$t(periodSummary.principleNameKey, periodSummary.principleNameKey)}}</th>
       </tr>
-      <tr class="row">
+      <tr :class="{'show': expanded}" class="row collapse">
         <th class="col-sm-3" scope="col">{{$t("action")}}</th>
         <th class="col-sm-5" scope="col">{{$t("description")}}</th>
         <th class="col-sm-2" scope="col">{{$t("date")}}</th>
         <th class="col-sm-2 align-right" scope="col">{{$t("investedMoney")}}</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody :class="{'show': expanded}" class="collapse">
       <tr v-for="action in periodSummary.actions" :key="action.name" class="row">
         <td class="col-sm-3">{{action.name}}</td>
         <td class="col-sm-5">{{action.description}}</td>
@@ -39,7 +39,12 @@ export default {
     formatNumber(number) {
       return parseNumber(number, this.$i18n.locale());
     }
-  }  
+  },
+  data() {
+    return {
+      expanded: true
+    }
+  }
 };
 </script>
 
