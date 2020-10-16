7ab34ce380ccc5ed4e334c1165ea54d0c4552f85
lferraro <lferraro@fiqus.coop>
2020-10-16 11:18:58 -0300

    componente para usar en lugar de los errores genericos de falta de data

---
+++ frontend/src/components/missing-data-empty-state.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="!hasPeriod || !hasActions" class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-12 col-sm-4">
+            <div class="mx-auto">
+                <img src="images/Ilustracion_COOBS_home_01.png" class="w-100 img-fluid mb-3">
+                <div v-if="!hasPeriod" class="row justify-content-center">
+                    <h1 class="h4 text-center mb-3">Aún no se ha creado ningun período</h1>
+                    <button type="button" class="btn btn-primary mb-3 mb-sm-0" @click.stop="$router.go(-1)">
+                        Crear un período
+                    </button>
+                </div>
+                <div v-else class="row justify-content-center">
+                    <h1 class="h4 text-center mb-3">Aún no se ha creado ninguna acción para el período seleccionado</h1>
+                    <button type="button" class="btn btn-primary mb-3 mb-sm-0" @click.stop="$router.go(-1)">
+                        Crear una acción
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        hasPeriod: {
+            type: Boolean,
+            default: false
+        },
+        hasActions: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
