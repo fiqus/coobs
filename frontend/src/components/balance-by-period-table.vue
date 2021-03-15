b8e5f569c065e99295ddbdcf509b4555104e4245
Diego Calero <dcalero@fiqus.coop>
2021-03-15 18:18:12 -0300

    [#308] Fixing social balance principle folding.

--- frontend/src/components/balance-by-period-table.vue
+++ frontend/src/components/balance-by-period-table.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="container-fluid">
   <table class="table table-hover">
-    <thead>
-      <tr class="row thead-light cursorPointer" @click.stop="expanded = !expanded">
+    <thead class="cursorPointer" @click.stop="toggle(periodSummary.principleNameKey)">
+      <tr class="row thead-light">
         <th v-if="groupedBy === 'sdg'" class="col-sm-12" scope="colgroup" colspan="4">{{$t(periodSummary.objectiveNameKey, periodSummary.objectiveNameKey)}}</th>
         <th v-if="groupedBy !== 'sdg'" class="col-sm-12" scope="colgroup" colspan="4">{{$t(periodSummary.principleNameKey, periodSummary.principleNameKey)}}</th>
       </tr>
-      <tr :class="{'show': expanded}" class="row collapse">
+      <tr :class="['collapse-'+periodSummary.principleNameKey]" class="row">
         <th class="col-sm-2" scope="col">{{$t("date")}}</th>
         <th class="col-sm-2" scope="col">{{$t("action")}}</th>
         <th class="col-sm-4" scope="col">{{$t("description")}}</th>
@@ -14,8 +14,8 @@
         <th class="col-sm-2 align-right" scope="col">{{$t("investedMoney")}}</th>
       </tr>
     </thead>
-    <tbody :class="{'show': expanded}" class="collapse">
-      <tr v-for="action in periodSummary.actions" :key="action.name" class="row">
+    <tbody>
+      <tr v-for="action in periodSummary.actions" :key="action.name" :class="['collapse-'+periodSummary.principleNameKey]" class="row">
         <td class="col-sm-2">{{action.date | formatToUIDate}}</td>
         <td class="col-sm-2">{{action.name}}</td>
         <td class="col-sm-4" v-html="action.description"></td>
@@ -45,13 +45,15 @@ export default {
     })
   },
   methods: {
+    toggle(key) {
+      $(".collapse-"+key).slideToggle();
+    },
     formatNumber(number) {
       return parseNumber(number, this.$i18n.locale());
     }
   },
   data() {
     return {
-      expanded: true
     }
   }
 };
