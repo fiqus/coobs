097a513633bc569f26d07f64ebb9518996e97613
Diego Calero <dcalero@fiqus.coop>
2021-06-10 13:03:43 -0300

    Little fix to ensure items poll on scroll at public actions feed.

--- frontend/src/components/infinite-scroll.vue
+++ frontend/src/components/infinite-scroll.vue
@@ -94,7 +94,7 @@ export default {
     },
     onScroll(ev) {
       const scrollOrWheel = ev.deltaY === undefined || ev.deltaY > 0;
-      const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+      const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight > document.documentElement.offsetHeight - 5;
       if (bottomOfWindow && scrollOrWheel && !this.noMoreData && !this.loading) {
         this.getMore();
       }
