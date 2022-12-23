430fd34669f8864c748d7a6fcd09cb44d9ee4d25
Nicolas Dimarco <nicolasdimarco@fiqus.coop>
2022-12-23 11:57:43 -0300

    issue solved: dashboard not working when no current period active found.

--- api/views/views_utils.py
+++ api/views/views_utils.py
@@ -4,18 +4,15 @@ from django.core.mail import EmailMultiAlternatives
 
 def get_current_period(all_periods):
     """
-    #FIXME not working
-    get_current_period:
     Returns the current period for a cooperative id based on today date.
     """
     today = datetime.today()
     # Note that if there are two periods that overlap, it returns the last one.
-    current_periods = [period for period in all_periods if
-                        datetime.strptime(period['date_from'], '%Y-%m-%d') < today < datetime.strptime(
+    periods = [period for period in all_periods if
+                        datetime.strptime(period['date_from'], '%Y-%m-%d') <= today <= datetime.strptime(
                             period['date_to'], '%Y-%m-%d')]
-    current_period = current_periods[len(current_periods) - 1]
-    if (current_period):
-        return current_period
+    if periods: 
+        return periods[len(periods) - 1]
     return None
 
 def create_and_send_email(subject, text_content, html_content, destination_emails):
